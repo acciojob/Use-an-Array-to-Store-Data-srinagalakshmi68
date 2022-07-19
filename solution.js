65f2f11d8d653340e3f181ed3a8c4c7d109e0da5
Srinagalakshmi <srinagalakshmi68@.com>
2022-07-19 19:23:24 +0530

    assignment added

--- solution.js
+++ solution.js
@@ -1,7 +1,16 @@
-
 let yourArray;
+yourArray = new Array(5);
+yourArray[0] = "one";
+yourArray[1] = 2;
+yourArray[2] = true;
+yourArray[3] = false;
+yourArray[4] = "raju";
+
+
 // assign your array the correct value in the next line
 // your code here!
 // code end
+
+
 
 module.exports = yourArray
